42ee0eea19d587e5962df79852b705dfe97f0bb3
Vladislav Evtushenko <vladislav.evtushenko@arcadia.spb.ru>
2020-01-14 20:59:24 +0300

    removed todolist

--- TodoList/Todolist.Core/Entities/BaseEntity.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace TodoList.Core.Entities
-{
-    public abstract class BaseEntity
-    {
-        public int Id { get; set; }
-    }
-}
--- TodoList/Todolist.Web/Models/ToDoItemDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TodoList.Web.Models
-{
-    public class ToDoItemDTO
-    {
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public bool IsDone { get; set; }
-    }
-}
